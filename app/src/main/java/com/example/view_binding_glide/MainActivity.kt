95e7dd6af8b31f5ef16efc879a9e3d812f283235
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-01-28 21:26:12 +0530

    completed today task

--- app/src/main/java/com/example/view_binding_glide/MainActivity.kt
+++ app/src/main/java/com/example/view_binding_glide/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.view_binding_glide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.example.view_binding_glide.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val url = "https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014_1280.jpg"
+        binding.changeButton.setOnClickListener {
+            Glide
+                .with(this)
+                .load(url)
+                .centerCrop()
+//                .placeholder(R.drawable.loading_spinner)
+                .into(binding.setImage);
+            binding.textStatus.text = "Image Changed"
+        }
     }
 }
